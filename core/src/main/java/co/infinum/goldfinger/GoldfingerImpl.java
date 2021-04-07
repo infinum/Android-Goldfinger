a2a92c7aaa4e52d5d8e0fab587f8c44d43de5770
Renato Turic <returic@gmail.com>
2021-04-07 11:32:59 +0200

    Remove resolved todo

--- core/src/main/java/co/infinum/goldfinger/GoldfingerImpl.java
+++ core/src/main/java/co/infinum/goldfinger/GoldfingerImpl.java
@@ -151,8 +151,6 @@ class GoldfingerImpl implements Goldfinger {
             return true;
         }
 
-        // TODO add control for supported auth combination
-
         if (!hasFingerprintHardware(params.allowedAuthenticators())) {
             callback.onError(new MissingHardwareException());
             return true;
